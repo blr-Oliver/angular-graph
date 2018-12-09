bad5b9acaf9ac6f3d797e9ad22a72d7b2640062c
blr-Oliver <vasily.liaskovsky@gmail.com>
2018-12-09 21:12:15 +0300

    working implementation of graph

--- src/ng/app/ui/graph-view.component.ts
+++ src/ng/app/ui/graph-view.component.ts
@@ -1,23 +1,22 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {DataRecord} from '../model/DataRecord';
 
+type RoundDirection = 'floor' | 'ceil' | 'round';
+
 @Component({
   selector: 'graph-view',
   templateUrl: './graph-view.component.html',
   styleUrls: ['./graph-view.component.scss']
 })
 export class GraphViewComponent implements OnInit {
+  private static readonly RATIO = 1.6;
   @Input() data: DataRecord[];
   categories: string[];
   series: number[][];
   yMax: number;
-
-  private static calcGoodYMax(maxValue: number): number {
-    const upperBound = maxValue * 1.1;
-    const scale = Math.floor(Math.log10(upperBound));
-    const multipler = Math.pow(10, scale);
-    return Math.ceil(upperBound / multipler) * multipler;
-  }
+  yUnit: number;
+  width: number;
+  height: number;
 
   ngOnInit(): void {
     this.series = [[], []];
@@ -27,6 +26,16 @@ export class GraphViewComponent implements OnInit {
       this.series[1][i] = r.avgScore;
     });
     const maxValue = Math.max(...this.series[0], ...this.series[1]);
-    this.yMax = GraphViewComponent.calcGoodYMax(maxValue);
+    this.width = this.categories.length * 100 + 20;
+    this.height = this.width / GraphViewComponent.RATIO;
+    this.yMax = this.roundSpecial(maxValue * 1.1, 1, 'ceil');
+    this.yUnit = this.roundSpecial((this.height - 20) / this.yMax, 2);
+    this.height = Math.ceil(this.yUnit * this.yMax) + 20;
+  }
+
+  private roundSpecial(value: number, significantDigits: number, direction: RoundDirection = 'round'): number {
+    const scale = Math.floor(Math.log10(Math.abs(value))) + (1 - significantDigits);
+    const multipler = Math.pow(10, scale);
+    return Math[direction](value / multipler) * multipler;
   }
 }
