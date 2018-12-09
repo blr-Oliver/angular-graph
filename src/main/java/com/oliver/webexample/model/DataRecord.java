5fd79cc64fa501188cebadca1ed1fd3d0dca71bd
blr-Oliver <vasily.liaskovsky@gmail.com>
2018-12-09 17:23:30 +0300

    implemented simplest CSV parser

---
+++ src/main/java/com/oliver/webexample/model/DataRecord.java
@@ -0,0 +1,16 @@
+package com.oliver.webexample.model;
+
+public class DataRecord {
+  private String words;
+  private Integer score;
+  private Float avgScore;
+
+  public String getWords() { return words; }
+  public void setWords(String words) { this.words = words; }
+
+  public Integer getScore() { return score; }
+  public void setScore(Integer score) { this.score = score; }
+
+  public Float getAvgScore() { return avgScore; }
+  public void setAvgScore(Float avgScore) { this.avgScore = avgScore; }
+}
---
+++ src/main/java/com/oliver/webexample/service/DataService.java
@@ -0,0 +1,51 @@
+package com.oliver.webexample.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.springframework.stereotype.Service;
+
+import com.oliver.webexample.model.DataRecord;
+
+@Service
+public class DataService {
+
+  private String pathToFile = "F:/ecl/workspace/simple-web-example/etc/Test-Data.csv";
+
+  public List<DataRecord> getData() throws IOException {
+    return loadAndParseData();
+  }
+
+  private List<DataRecord> loadAndParseData() throws IOException {
+    Scanner scanner = null;
+    try {
+      scanner = new Scanner(new File(pathToFile));
+      String[] header = scanner.nextLine().split("\\s*,\\s*", -1);
+      List<DataRecord> result = new ArrayList<>();
+      while (scanner.hasNextLine()) {
+        final String line = scanner.nextLine();
+        if (!line.isEmpty())
+          result.add(fromRawData(line.split("\\s*,\\s*", header.length))); // limit keeps empty fragments
+      }
+      return result;
+    } finally {
+      if (scanner != null)
+        scanner.close();
+    }
+  }
+
+  private DataRecord fromRawData(String[] data) {
+    DataRecord result = new DataRecord();
+    if (!data[0].isEmpty())
+      result.setWords(data[0]);
+    if (!data[1].isEmpty())
+      result.setScore(Integer.parseInt(data[1]));
+    if (!data[2].isEmpty())
+      result.setAvgScore(Float.parseFloat(data[2]));
+    return result;
+  }
+
+}
