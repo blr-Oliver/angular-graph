0f7a8904b09a6deeeae9f4f011a9e9022ced6917
blr-Oliver <vasily.liaskovsky@gmail.com>
2019-08-18 17:03:03 +0300

    compatible both deploy and local base href

--- src/ng/app/app.module.ts
+++ src/ng/app/app.module.ts
@@ -1,3 +1,4 @@
+import {APP_BASE_HREF, PlatformLocation} from '@angular/common';
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
@@ -6,6 +7,15 @@ import {TabularViewComponent} from './ui/tabular-view.component';
 import {HttpClientModule} from '@angular/common/http';
 import {DataService} from './service/DataService';
 import {GraphViewComponent} from './ui/graph-view.component';
+
+// from https://stackoverflow.com/questions/39287444/angular2-how-to-get-app-base-href-programmatically#answer-46493276
+export function getBaseHref(platformLocation: PlatformLocation): string {
+  const current = platformLocation.getBaseHrefFromDOM();
+  if (!current || current === '/') {
+    return '';
+  }
+  return current;
+}
 
 @NgModule({
   declarations: [
@@ -18,7 +28,12 @@ import {GraphViewComponent} from './ui/graph-view.component';
     HttpClientModule
   ],
   providers: [
-    DataService
+    DataService,
+    {
+      provide: APP_BASE_HREF,
+      useFactory: getBaseHref,
+      deps: [PlatformLocation]
+    }
   ],
   bootstrap: [AppComponent]
 })
--- src/ng/app/service/DataService.ts
+++ src/ng/app/service/DataService.ts
@@ -1,14 +1,15 @@
-import {Injectable} from '@angular/core';
+import {APP_BASE_HREF} from '@angular/common';
+import {Inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Point} from '../model/Point';
 
 @Injectable()
 export class DataService {
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, @Inject(APP_BASE_HREF) private baseHref: string) {
   }
 
   getData(): Observable<Point[]> {
-    return this.http.get<Point[]>('/data/sample.json');
+    return this.http.get<Point[]>(`${this.baseHref}/data/sample.json`);
   }
 }
